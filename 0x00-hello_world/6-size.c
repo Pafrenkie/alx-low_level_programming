03ccedd76fc7a7373ead44670943bf3b5ed91c12
pafrenkie <francispapoi61@gmail.com>
2023-08-10 13:54:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,13 +12,13 @@ printf("Running on a 64-bit machine\n");
 #else
 printf("Running on a 32-bit machine\n");
 #endif
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of short int: %zu byte(s)\n", sizeof(short int));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
-printf("Size of double: %zu byte(s)\n", sizeof(double));
-printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of short int: %lu byte(s)\n", sizeof(short int));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of double: %lu byte(s)\n", sizeof(double));
+printf("Size of long double: %lu byte(s)\n", sizeof(long double));
 return (0);
 }
