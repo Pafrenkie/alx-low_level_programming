068fcfccdb1f8f2f9f8e51e49b34b064c4165a2a
pafrenkie <francispapoi61@gmail.com>
2023-08-12 14:59:27 -0700

    ./7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Pro Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char  d;
+d = 'z';
+
+while
+(d >= 'a')
+{
+putchar(d);
+d--;
+}
+putchar('\n');
+return (0);
+}
