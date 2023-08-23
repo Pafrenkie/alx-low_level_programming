fe210911a38675b96fab42d3943c2b903d803d5e
pafrenkie <francispapoi61@gmail.com>
2023-08-23 13:27:19 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+    if (n == 0)
+    {
+        _putchar('0');
+        return;
+    }
+
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+
+    int num_digits = 0;
+    int temp = n;
+    while (temp != 0)
+    {
+        temp /= 10;
+        num_digits++;
+    }
+
+    int divisor = 1;
+    for (int i = 1; i < num_digits; i++)
+    {
+        divisor *= 10;
+    }
+
+    while (divisor != 0)
+    {
+        _putchar((n / divisor) + '0');
+        n %= divisor;
+        divisor /= 10;
+    }
+}
