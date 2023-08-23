ad79c3836602f86eeec2694a34fdcf9aa52c21b6
pafrenkie <francispapoi61@gmail.com>
2023-08-23 13:14:58 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,45 +1,38 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * _strncpy - Copies up to n bytes of src to dest.
- * @dest: The destination string.
- * @src: The source string to be copied.
- * @n: The maximum number of bytes to copy from src.
- *
- * Return: A pointer to the resulting string @dest.
- */
+
 
 int main(void)
 {
-char dest[20] = "Hello";
-char src[] = "world!";
-int n = 3;
+    char dest[20] = "Hello";
+    char src[] = "world!";
+    int n = 3;
 
-printf("Before copying: %s\n", dest);
-_strncpy(dest, src, n);
-printf("After copying: %s\n", dest);
+    printf("Before copying: %s\n", dest);
+    _strncpy(dest, src, n);
+    printf("After copying: %s\n", dest);
 
-return (0);
+    return 0;
 }
 
 char *_strncpy(char *dest, char *src, int n)
 {
-char *ptr = dest;
-int i;
+    char *ptr = dest;
+    int i;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-*ptr = src[i];
-ptr++;
-}
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        *ptr = src[i];
+        ptr++;
+    }
 
-for (; i < n; i++)
-{
-*ptr = '\0';
-ptr+;
-}
+    for (; i < n; i++)
+    {
+        *ptr = '\0';
+        ptr++;
+    }
 
-return (dest);
+    return dest;
 }
 
