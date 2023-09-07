4f0610b2ef8f90482f8028140cc4b322af9c3e84
pafrenkie <francispapoi61@gmail.com>
2023-09-07 11:51:16 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size) {
+void *ptr;
+
+if (nmemb == 0 || size == 0) {
+return NULL;
+}
+
+ptr = malloc(nmemb * size);
+
+if (ptr == NULL) {
+return NULL;
+}
+
+memset(ptr, 0, nmemb * size);
+
+return ptr;
+}
