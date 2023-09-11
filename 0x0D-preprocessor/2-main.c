6dca695ecd1c84663d65d272e80e37eef3003af4
pafrenkie <francispapoi61@gmail.com>
2023-09-11 05:08:12 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * prints the name of the file it was compiled from
+ * followed by a new line.
+ * Return 0
+ */
+
+int main(void)
+{
+printf(__FILE__ "\n");
+return (0);
+}
